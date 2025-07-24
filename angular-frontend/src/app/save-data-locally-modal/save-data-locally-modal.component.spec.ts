0f167a3f3a5cb79acc1f4118af82ccc5c590ec55
franzwil <franz.wilfarth@mail.de>
2025-07-24 09:52:15 +0200

    Add Spy object to test file for save-data-locally-modal

--- angular-frontend/src/app/save-data-locally-modal/save-data-locally-modal.component.spec.ts
+++ angular-frontend/src/app/save-data-locally-modal/save-data-locally-modal.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SaveDataLocallyModalComponent } from './save-data-locally-modal.component';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('SaveDataLocallyModalComponent', () => {
   let component: SaveDataLocallyModalComponent;
   let fixture: ComponentFixture<SaveDataLocallyModalComponent>;
+  const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SaveDataLocallyModalComponent]
+      imports: [SaveDataLocallyModalComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefSpy }
+      ]
     })
       .compileComponents();
 
